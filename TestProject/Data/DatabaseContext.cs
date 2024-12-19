85a192e13d021534fe46847b66a241dd562620c6
Andrey <andreiten1@gmail.com>
2024-12-19 03:21:30 +0300

    fix

--- TestProject/Data/DatabaseContext.cs
+++ TestProject/Data/DatabaseContext.cs
@@ -5,9 +5,16 @@ namespace TestProject.Data
 {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
 
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<RecipeStructure> RecipeStructures { get; set; }
+        public DbSet<Component> Component { get; set; }
+        public DbSet<ComponentType> ComponentTypes { get; set; }
+        public DbSet<RecipeMixerSet> RecipeMixerSets { get; set; }
+        public DbSet<RecipeTimeSet> RecipeTimeSets { get; set; }
     }
 }
